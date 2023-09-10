9f3e770d15f4ef2ca894d041d6e2024d6e4c87a2
Anna Levenberg <alevenb@google.com>
2023-09-10 17:49:44 -0400

    build

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,10 @@
 #include <iostream>
-#include "gen/message_generator.pb.h"
+
+#include <message_generator/message_generator.pb.h>
 
 int main(int argc, char** argv) {
-    PublishMessageRequest request;
+    alevenb::PublishMessageRequest request;
     request.set_id("123");
     std::cout << "Hello World!" << std::endl;
-    std::cout << request.DebugString(); << std::endl;
+    std::cout << request.DebugString() << std::endl;
 }
